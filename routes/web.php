38c679bc80fc84de5b50977a36631155b1ed25e3
Lee <377737058@qq.com>
2019-06-12 18:47:41 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::resource('users','UsersController')->only(['show','edit','update']);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+//  文件上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
